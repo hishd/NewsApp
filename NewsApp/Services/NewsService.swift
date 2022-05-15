aaf7819126411957f33be8b103150e55f2e2be15
Hishara Dilshan <hisharadilshan3@gmail.com>
2022-05-15 22:52:42 +0530

    Updated the NewsService functions to Future Publisher on Combine. Added Generics for the Decodable Types

--- NewsApp/Services/NewsService.swift
+++ NewsApp/Services/NewsService.swift
@@ -6,14 +6,24 @@
 //
 
 import Foundation
+import Combine
 
 enum NetworkError: Error {
     case badUrl
+    case responseError
     case invalidData
     case decodingError
+    case unknown
 }
 
 class NewsService {
+    
+    static let shared = NewsService()
+    
+    private init() {}
+    
+    private var cancellables = Set<AnyCancellable> ()
+    
     func fetchSources(url: URL?, completion: @escaping (Result<[NewsSource], NetworkError>) -> Void) {
         guard let url = url else {
             completion(.failure(.badUrl))
@@ -46,5 +56,39 @@ class NewsService {
             completion(.success(newsArticles?.articles ?? []))
             return
         }.resume()
+    }
+    
+    func getData<T: Decodable>(from url: URL?, type: T.Type) -> Future<[T], Error> {
+        return Future<[T], Error> { promise in
+            guard let url = url else {
+                return promise(.failure(NetworkError.badUrl))
+            }
+            
+            URLSession.shared.dataTaskPublisher(for: url)
+                .tryMap { (data, response) in
+                    guard let response = response as? HTTPURLResponse, 200...299 ~= response.statusCode else {
+                        throw NetworkError.responseError
+                    }
+                    return data
+                }
+                .decode(type: [T].self, decoder: JSONDecoder())
+                .receive(on: RunLoop.main)
+                .sink { completion in
+                    if case let .failure(error) = completion {
+                        switch error {
+                        case let decodingError as DecodingError:
+                            promise(.failure(decodingError))
+                        case let apiError as NetworkError:
+                            promise(.failure(apiError))
+                        default:
+                            promise(.failure(NetworkError.unknown))
+                        }
+                    }
+                } receiveValue: { result in
+                    promise(.success(result))
+                }
+                .store(in: &self.cancellables)
+
+        }
     }
 }
